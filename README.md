ea7d5ebd979fd8a70845c6ca5475b4e3dfa7b279
Eric Iacutone <eric.iacutone@gmail.com>
2016-05-20 18:49:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://bl.ocks.org/mbostock/3887193
+http://www.adeveloperdiary.com/d3-js/create-a-simple-donut-chart-using-d3-js/
+http://zeroviscosity.com/d3-js-step-by-step/step-2-a-basic-donut-chart
---
+++ app.js
@@ -0,0 +1,38 @@
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#7E7E7F", "#DAE3FF", "#FBFCFF", "#73767F", "#C9CACC"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.card_count; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data.csv", function(error, data) {
+  if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.board); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.board + " " + d.data.card_count; });
+});
