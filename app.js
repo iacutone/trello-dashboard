977202331e4aa57a35a5fe0c7de88cc4fe5fdf46
Eric Iacutone <eric.iacutone@gmail.com>
2016-05-22 12:41:41 -0300

    basic designs

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var color = d3.scale.ordinal()
     .range(["#7E7E7F", "#DAE3FF", "#FBFCFF", "#73767F", "#C9CACC"]);
 
 var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 70);
+    .outerRadius(radius)
+    .innerRadius(radius - 80);
 
 var pie = d3.layout.pie()
     .sort(null)
@@ -31,8 +31,32 @@ d3.csv("data.csv", function(error, data) {
       .attr("d", arc)
       .style("fill", function(d) { return color(d.data.board); });
 
+  // g.append("text")
+  //     .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+  //     .text(function(d) { return d.data.board });
+
   g.append("text")
       .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
       .attr("dy", ".35em")
-      .text(function(d) { return d.data.board + " " + d.data.card_count; });
+      .text(function(d) { return d.data.card_count; });
+
+  var legend = d3.select("body").append("svg")
+      .attr("class", "legend")
+      .attr("width", radius * 2)
+      .attr("height", radius * 2)
+    .selectAll("g")
+      .data(color.domain().slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(function(d) { return d; });
 });
